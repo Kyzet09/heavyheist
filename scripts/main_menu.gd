0e932bcd24675ab3d8453102824daead74cacab3
Qbrnx <qbrnx@MacBook-Pro-de-Qbrnx.local>
2026-06-07 13:46:40 -0400

    fix pb button ios

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,9 +2,5 @@ extends Node2D
 
 @onready var button_play: Button = $VBoxContainer/Button_play
 
-
-func _ready():
-	button_play.pressed.connect(_on_play_pressed)
-
-func _on_play_pressed():
+func _on_button_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Tuto.tscn")
