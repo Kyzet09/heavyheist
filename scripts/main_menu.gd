ba17d38b4c1ed55f78abbd749fca263b3b725bec
Qbrnx <qbrnx@MacBook-Pro-de-Qbrnx.local>
2026-06-07 14:28:53 -0400

    fix bug buttons ios

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,4 +3,4 @@ extends Node2D
 @onready var button_play: Button = $VBoxContainer/Button_play
 
 func _on_button_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Tuto.tscn")
+	get_tree().change_scene_to_file("res://scenes/tuto.tscn")
