18edc63cfc254950950210812e8aa590fbdee9a3
Qbrnx <qbrnx@MacBook-Pro-de-Qbrnx.local>
2026-06-08 19:20:24 -0400

    bouton skip tuto

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_button_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tuto.tscn")
+
+
+func _on_button_skip_tuto_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
